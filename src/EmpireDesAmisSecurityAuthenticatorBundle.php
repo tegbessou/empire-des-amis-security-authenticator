c4e08066d412757561ef361e0518343dd66f3a36
Tegbessou <hugues.gobet@gmail.com>
2024-12-10 20:46:39 +0100

    Setup bundle #major

---
+++ src/EmpireDesAmisSecurityAuthenticatorBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EmpireDesAmis\SecurityAuthenticatorBundle;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class EmpireDesAmisSecurityAuthenticatorBundle extends AbstractBundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $loader = new YamlFileLoader($builder, new FileLocator(__DIR__.'/../config'));
+        $loader->load('services.yaml');
+    }
+}
